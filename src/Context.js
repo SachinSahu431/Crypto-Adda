32e72fa8c0c4d237f0775d43d2b70e158a703bf9
Sachin Sahu <sachinksahu.431@gmail.com>
2022-01-22 15:19:49 +0530

    Header complete

---
+++ src/Context.js
@@ -0,0 +1,28 @@
+import React, { createContext, useContext, useState, useEffect } from "react";
+
+// name of the state
+const crypto = createContext();
+
+const Context = ({ children }) => {
+  const [currency, setCurrency] = useState("INR");
+  const [symbol, setSymbol] = useState("₹");
+
+  useEffect(() => {
+    if (currency == "INR") setSymbol("₹");
+    else if (currency == "USD") setSymbol("$");
+  }, [currency]);
+
+  //   currency is dependency here
+
+  return (
+    <crypto.Provider value={{ currency, symbol, setCurrency }}>
+      {children}
+    </crypto.Provider>
+  );
+};
+
+export default Context;
+
+export const CryptoState = () => {
+  return useContext(crypto);
+};
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,78 @@
 // rafce - react arrow function export component
 
+import {
+  AppBar,
+  Container,
+  Select,
+  Toolbar,
+  MenuItem,
+  Typography,
+  createTheme,
+  ThemeProvider,
+} from "@material-ui/core";
+import { makeStyles } from "@material-ui/styles";
 import React from "react";
+import { useHistory } from "react-router-dom";
+import { CryptoState } from "../Context";
+
+const useStyles = makeStyles(() => ({
+  title: {
+    flex: 1,
+    color: "gold",
+    fontFamily: "Montserrat",
+    fontWeight: "bold",
+    cursor: "pointer",
+  },
+}));
 
 const Header = () => {
+  const classes = useStyles();
+
+  const { currency, setCurrency } = CryptoState();
+
+  console.log(currency);
+
+  // to navigate between pages
+  const history = useHistory();
+
+  const darkTheme = createTheme({
+    palette: {
+      primary: {
+        main: "#fff",
+      },
+      type: "dark",
+    },
+  });
+
   return (
-    <div>
-      <h1>header</h1>
-    </div>
+    <ThemeProvider theme={darkTheme}>
+      <AppBar color="transparent" position="static">
+        <Container>
+          <Toolbar>
+            <Typography
+              onClick={() => history.push("/")}
+              className={classes.title}
+              variant="h6"
+            >
+              Cheems Hub
+            </Typography>
+
+            <Select
+              variant="outlined"
+              style={{
+                width: 100,
+                height: 40,
+                marginRight: 15,
+              }}
+              onChange={(e) => setCurrency(e.target.value)}
+            >
+              <MenuItem value="USD">USD</MenuItem>
+              <MenuItem value="IND">IND</MenuItem>
+            </Select>
+          </Toolbar>
+        </Container>
+      </AppBar>
+    </ThemeProvider>
   );
 };
 
