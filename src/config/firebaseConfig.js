3c3e32f0d9f0dfb65d1f980a0c33ad2dfce90d94
Sachin Sahu <sachinksahu.431@gmail.com>
2022-07-05 17:23:01 +0530

    Add Firebase

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBjC2Py-a7PlJkPBHVxa6Onup-PhhbIlyI",
+  authDomain: "crypto-adda-edb5f.firebaseapp.com",
+  projectId: "crypto-adda-edb5f",
+  storageBucket: "crypto-adda-edb5f.appspot.com",
+  messagingSenderId: "465790408886",
+  appId: "1:465790408886:web:42e72bb8f8d16515012e17",
+};
+
+export default firebaseConfig;
